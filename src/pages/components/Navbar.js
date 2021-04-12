8c0ea788aff27a2d193e9b654dc800061b61b330
AndrosGreen <gclandres@gmail.com>
2021-04-11 23:20:55 -0500

    Login y Logout super admin, Mejoras al Navbar

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import {Link,withRouter} from 'react-router-dom';
+import sigess from '../api/sigess';
 
 class Navbar extends React.Component {
+    
+    logOut = async () => {
+        const response = await sigess.get("/usuarios/logout",
+            {}
+        );
+        console.log(response);
+        localStorage.removeItem("token");
+        this.props.history.push("/");
+    }
+
     render(){
+        
+        let isAuth = localStorage.getItem("token");
+        
         return ( 
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#198b34"}}>
@@ -17,13 +31,28 @@ class Navbar extends React.Component {
 
                         <div className="collapse navbar-collapse" id="navbarScroll">
                             <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" to="/whatup"> 404 NOT FOUND </Link>
-                                </li>
+
+                                { (isAuth === "1234")? (
+                                        <li className="nav-item">
+                                            <Link className="nav-link active" to="/gestionar/usuarios"> Gestionar Usuarios </Link>
+                                        </li>        
+                                    ): (
+                                        <li className="nav-item">
+                                            <Link className="nav-link active" to="/whatup"> 404 NOT FOUND </Link>
+                                        </li>
+                                    )
+                                }
+
                             </ul>
 
                             <div className="d-flex">
-                                <button className="btn btn-light"> Log in </button>
+                                { (isAuth === "1234" )? (
+                                        <button className="btn btn-light" onClick={this.logOut}>Log Out</button>
+                                    ): (
+                                        <Link className="btn btn-light" to="/login">Log in</Link>
+                                    )
+                                }
+                                
                             </div>                           
 
                         </div>
