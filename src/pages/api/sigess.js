e550e45cd9924458c189c9d5a6b61035134af2ba
DT3264 <cernadaniel32@gmail.com>
2021-04-12 22:47:42 -0500

    Actualizado para mantener sesion

--- src/pages/api/sigess.js
+++ src/pages/api/sigess.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
+import axiosCookieJarSupport from 'axios-cookiejar-support';
+import tough from 'tough-cookie';
 
-export default axios.create({
-    baseURL: 'http://127.0.0.1:5000/'
+const cookieJar = new tough.CookieJar();
+
+const cosa = axios.create({
+    baseURL: 'http://127.0.0.1:5000/',
+    withCredentials: true,
+    jar: cookieJar
 });
+
+axiosCookieJarSupport(cosa);
+
+export default cosa;
