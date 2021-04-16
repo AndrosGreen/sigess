1b079c757f66138dfc42222aa26a12a95bfbff00
Frank55-T <66974093+Frank55-T@users.noreply.github.com>
2021-04-16 10:58:43 -0500

    CrearAlumno, Eliminar Alumno, Modificar Alumno, Listar Alumnos y Registrar Alumnos
    Crud de Alumno en el controlador de Alumno, metodo para listar Alumnos y método para registrar alumnos si ya hay uno registrado

--- backend/Controladores/ControladorAlumnos.py
+++ backend/Controladores/ControladorAlumnos.py
@@ -1,26 +1,51 @@
 from Modelos.AlumnoPreRegistro import AlumnoPreRegistro
+from Modelos.Alumno import Alumno
 from sigess.db import executeStatement, executeQueryWithData, executeQuery
 
+def obtenAlumnoPorNombre(nombreAlumno):
+    """Obtiene un alumno dado un nombre"""
+    sql = "select * from alumnos where nombre = %s limit 1"
+    data = nombreAlumno
+    filas = executeQueryWithData(sql, data)
+    if len(filas) == 0:
+        return None
+    else:
+        fila = filas[0]
+        return Alumno.desdeFila(fila)
 
-def creaAlumno():
-    pass
+def creaAlumno(alumno):
+    """Crea un alumno y lo devuelve si sale bien o None si no"""
+    sql = "insert into alumnos values(%s, %s, %s, %s, %s, %s, %s, %s)"
+    data = (alumno.noControl, alumno.nombre, alumno.apPaterno, alumno.apMaterno, alumno.correo, alumno.clave, alumno.carrera,alumno.programa, alumno.encargado, alumno.empresa)
+    executeStatement(sql, data)
+    return obtenAlumnoPorNombre(alumno.nombre)
 
 
-def eliminaAlumno():
-    pass
+def eliminaAlumno(noControl):
+    """Elimina un alumno dado su id"""
+    sql = "delete from alumnos where noControl = %s"
+    data = noControl
+    executeStatement(sql, data)
 
 
-def modificaAlumno():
-    pass
+def modificaAlumno(alumno):
+    """Modifica un alumno"""
+    sql = 'update alumnos set nombre=%s, apPaterno=%s, apMaterno=%s, correo=%s, clave=%s, carrera=%s, programa=%s, encargado=%s, empresa=%s where noControl=%s'
+    data = (alumno.nombre, alumno.apPaterno, alumno.apMaterno, alumno.correo, alumno.clave, alumno.carrera, alumno.programa, alumno.encargado, alumno.empresa,alumno.noControl)
+    executeStatement(sql, data)
 
 
-def registraAlumno():
-    pass
+def registraAlumno(alumno):
+    """Checa si un alumno está preregistrado y si es así lo registra, sino regresa None"""
+    if existeAlumnoAPreRegistrar(alumno)==True:
+        creaAlumno(alumno)
+    else:
+        return None
 
 
 def listaAlumnos():
-    pass
-
+    rows = executeQuery("select * from alumnos")
+    return rows  # Ya tiene los datos del pokemon
 
 def existeAlumnoAPreRegistrar(alumno):
     """Consulta si existe el alumno a preregistrar en el pre registro o en los registros"""
