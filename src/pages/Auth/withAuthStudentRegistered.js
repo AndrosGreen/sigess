36e8059ef2e8d67997f71480f0777e10f6ae1805
AndrosGreen <gclandres@gmail.com>
2021-05-06 13:50:05 -0500

    Comentario arreglado

--- src/pages/Auth/withAuthStudentRegistered.js
+++ src/pages/Auth/withAuthStudentRegistered.js
@@ -1,5 +1,11 @@
+import React from "react";
 import { Redirect } from "react-router-dom";
 
+/**
+ * 
+ * @param {React.Component} Component - Componente a mostrar 
+ * @returns Componente depende si esta logueado y en caso contrario, notFound.
+ */
 const withAuthStudentRegistered = (Component) => {
     const AuthRoute = () => {
         const isAuth = JSON.parse( sessionStorage.getItem("usuario") );
