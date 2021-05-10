f5226368d2ff675f14689b604f90b66be5adf1ec
AlexiaAM <alexia77755@gmail.com>
2021-05-09 19:36:17 -0500

    prerrequisito avance

--- src/pages/ViewRequisitesStudent.js
+++ src/pages/ViewRequisitesStudent.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import sigess from './api/sigess';
+import RequisitoAlumnoAdminCard from './components/RequisitoAlumnoCard';
+import ListRequisitoAlumnos from './components/ListRequisitoAlumnos';
 
 class ViewRequisitesStudent extends React.Component {
-    render(){
-        return <div>ViewRequisitesStudent</div>;
-    }
-}
 
+    state = { 
+        // requisites
+        requisites : [],
+        requisiteName : '',
+        description : '',
+        status : ''
+
+    };
+
+    /**
+     * carga los requisitos que se encuentran en la bd
+     */
+    componentDidMount (){
+        //obtener el numero de control del alumno logeado
+        const usuario = JSON.parse( sessionStorage.getItem("usuario") );
+        let noControlAct = usuario.usuario;
+        console.log(noControlAct);
+        this.loadRequisites(noControlAct);
+    }
+
+    /*AuthRoute = () => {
+        const isAuth = JSON.parse( sessionStorage.getItem("usuario") );
+        if(isAuth.usuario === 0){
+            return <Component />
+        }
+        else {
+            return <Redirect to="/notFound"/>
+        }
+    };*/ 
+    
+    /**
+     * carga los requisitos de la bd
+     */
+    loadRequisites = async (noControl) => {
+        const respuesta = await sigess.get( '/requisitos/estatusAlumno',{
+            params : {
+                noControl : "S18120183"
+            }
+            }
+        );
+        console.log(respuesta);
+        //this.setState( { requisites : respuesta.data } );
+    }
+
+    render(){
+        return (
+            <div>
+                <h4 style={{marginBottom: "15px"}}>aqui va el nombre del alumno</h4>
+                <ListRequisitoAlumnos
+                    requisites = {this.state.requisites}
+                />
+                    
+            </div>
+            
+        );
+    } 
+}
+ 
 export default ViewRequisitesStudent;
--- src/pages/components/RequisitoAlumnoCard.js
+++ src/pages/components/RequisitoAlumnoCard.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 class RequisitoAlumnoCard extends React.Component {
+    state = {
+        status : ''
+    }
+
+    colorStatus(){
+        if(this.props.status == 'R'){
+            return this.state.status = "Rechazado";
+        }
+        else if(this.props.status == 'A'){
+            return this.state.status = "Aceptado";
+        }
+        else{
+            return this.state.status = "Pendiente";
+        }
+    }
+
     render(){
-        return <div>RequisitoAlumnoCard</div>;
+        return(
+            <div className = "card" style={{ width : "40rem" , marginTop : "10px" }}>
+                <div className="card-body">
+                    <h5 className="card-title"> {this.props.requisiteName} </h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Estatus : </h6>
+                    <button 
+                        className="btn btn-secondary" style={{margin : "10px"}}
+                    > 
+                        {this.colorStatus()}
+                    </button>
+                    <p class="card-text">{this.props.description}</p>
+                    
+                </div>
+            </div>
+        );
     }
 }
 
