7717b76c85668c5cce8b7c7a6b9cb9f4737d08ff
AlexiaAM <alexia77755@gmail.com>
2021-05-12 17:42:00 -0500

    Base y disenio asignacion

--- src/pages/VistaAsignacionesAlumno.js
+++ src/pages/VistaAsignacionesAlumno.js
@@ -1,9 +1,66 @@
 import React from 'react';
 import withAuthStudentRegistered from './Auth/withAuthStudentRegistered';
+import AsignacionAlumnoCard from './components/AsignacionAlumnoCard';
+import ListAsignacionAlumnos from './components/ListAsignacionAlumnos';
 
 class VistaAsignacionesAlumno extends React.Component {
+    state = { 
+        // asignations
+        assignments : [
+           
+        ],
+        assignmentName : '',
+        beginDate : '',
+        endDate : '',
+        stage : '',
+        noControl : '',
+        porCiento : 87
+
+    };
+
+    /**
+     * carga los requisitos que se encuentran en la bd
+     */
+    componentDidMount (){
+        //obtener el numero de control del alumno logeado
+        const usuario = JSON.parse( sessionStorage.getItem("usuario") );
+        let noControlAct = usuario.usuario;
+        this.setState({noControl : noControlAct})
+        //this.loadAsigments(noControlAct);
+    }
+    
+    /**
+     * carga las asignaciones del alumno
+     * @param {String} noControl 
+     */
+
+    /*loadAssignments = async (noControl) => {
+        const respuesta = await sigess.post( 'link a la peticion',{
+                noControl : noControl
+            }
+        );
+        console.log(respuesta.data);
+        //this.setState( { requisites : respuesta.data } );
+    }*/
+
     render(){
-        return <div>VistaAsignacionesAlumno</div>;
+        return (
+            <div>
+                <h4 style={{marginBottom: "15px"}}>{this.state.noControl}</h4>
+                <p>Progreso :</p>
+                <div class="progress" style={{marginTop : "1rem"}}>
+                    <div className="progress-bar bg-warning" role="progressbar" style={{width : `${this.state.porCiento}%`}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"> {this.state.porCiento}% </div>
+                </div>
+                <ListAsignacionAlumnos
+                    assignments = {this.state.assignments}
+                />
+                <AsignacionAlumnoCard
+
+                />
+                    
+            </div>
+            
+        );
     }
 }
 
--- src/pages/components/ListAsignacionAlumnos.js
+++ src/pages/components/ListAsignacionAlumnos.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import AsignacionAlumnoCard from './AsignacionAlumnoCard';
 
 class ListAsignacionAlumnos extends React.Component {
     render(){
-        return <div>ListAsignacionAlumnos</div>;
+        const assignments = this.props.assignments.map(
+            assignment => {
+                return (
+                    <AsignacionAlumnoCard
+                        asigmentName = {assignment.asigmentName}
+                        beginDate = {assignment.beginDate}
+                        endDate = {assignment.endDate}
+                        stage = {assignment.stage}
+                        noControl = {assignment.noControl}
+                        status = {assignment.status}
+                    />
+                );
+            }
+        );
+
+        return (
+            <div> {assignments} </div>
+        );
     }
 }
 
