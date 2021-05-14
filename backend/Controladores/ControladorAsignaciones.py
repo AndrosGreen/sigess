7213ae7b19814ac0531959315b81e1e8f9f65544
Frank55-T <66974093+Frank55-T@users.noreply.github.com>
2021-05-13 19:17:12 -0500

    RF_8 Monitorizar Alumnos
    Resumen del Alumno, cuantas tareas lleva y cuantas faltan :vvv

--- backend/Controladores/ControladorAsignaciones.py
+++ backend/Controladores/ControladorAsignaciones.py
@@ -16,3 +16,11 @@ def subirPDF(nombre, archivo):
 	sql = f"insert into documentosPrueba values(null, %s, %s)"
 	data = (nombre, archivo)
 	executeStatement(sql, data)
+
+def monitorearAlumnos():
+	"""Obtiene los alumnos y las tareas que han hecho y las que tienen en total"""
+	rows = executeQuery("select a.noControl, concat(a.nombre,' ', a.apPaterno,' ', a.apMaterno) as nombre, "
+						"count(a.noControl) as Tareas, (select count(l.Asignaciones_idAsignacion) from asign"
+						"acionesalumnos l where l.Alumnos_noControl=a.noControl) as Total from alumnos a nat"
+						"ural join asignacionesalumnos p where p.estado='A'")
+	return rows
